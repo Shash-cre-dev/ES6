763831ee3480a4e4038727f4c4d37d7ed15f836e
Shashank <shashwritescode@gmail.com>
2023-04-25 16:44:36 +0530

    Trailing Commas

---
+++ 15 Trailing Commas/index.js
@@ -0,0 +1,18 @@
+//TRAILING COMMAS
+
+//in the past wherever comma used if an element not present after it
+//then it throws an arrow
+
+//But in ES6, that issue is resolved 
+//no logical errors when elements are not placed after a comma 
+
+function Intro(param1,){
+    const example = {
+        name: 'Shashank',
+    };
+    
+    console.log(example)
+};
+
+Intro();
+//{name: "Shashank"}
